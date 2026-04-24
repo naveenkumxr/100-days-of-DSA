e85b31d72ef5c8d6872c3ca6c53f9ae8966e7939
NAVEEN KUMAR <nsheorann98@gmail.com>
2026-04-24 21:25:36 +0530

    Add files via upload

---
+++ q083.c
@@ -0,0 +1,84 @@
+/*
+Problem: Implement Selection Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+int main(void)
+{
+    int n;
+
+    printf("Enter number of elements: ");
+    if (scanf("%d", &n) != 1 || n <= 0)
+    {
+        return 0;
+    }
+
+    int *arr = (int *)malloc((size_t)n * sizeof(int));
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+        if (i < n - 1)
+        {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
+    free(arr);
+    return 0;
+}
